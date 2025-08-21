1e827372d89fb82a61d6d597014fbb71f533b327
Vince Chang <vince_chang@aspeedtech.com>
2025-08-21 13:54:16 +0800

    meta-aspeed-sdk: ast2700-dcscm: correct set-post-code-led get postcode
    Change awk command to extract the third field instead of the second when
    retrieving the postcode from busctl output. This ensures the correct value
    is used to set the GPIO for the initial postcode LED state.
    
    ```
    root@ast2700-dcscm:~# busctl get-property xyz.openbmc_project.State.Boot.Raw \
    /xyz/openbmc_project/state/boot/raw0  xyz.openbmc_project.State.Boot.Raw Value | awk '{print $3}'
    239
    ```
    
    Signed-off-by: Vince Chang <vince_chang@aspeedtech.com>
    Change-Id: I43f1d41119117292c69ff68ad07d7f84dced6349

--- meta-aspeed-sdk/meta-ast2700-pfr/recipes-phosphor/host/set-post-code-led/set-post-code-led.sh
+++ meta-aspeed-sdk/meta-ast2700-pfr/recipes-phosphor/host/set-post-code-led/set-post-code-led.sh
@@ -47,7 +47,7 @@ set_gpio() {
 # Initial GPIO value only power on.
 if [ "$(cat /sys/class/watchdog/watchdog0/bootstatus)" = "0" ]; then
     postcode=$(busctl get-property xyz.openbmc_project.State.Boot.Raw /xyz/openbmc_project/state/boot/raw0 \
-                xyz.openbmc_project.State.Boot.Raw Value | awk '{print $2}')
+                xyz.openbmc_project.State.Boot.Raw Value | awk '{print $3}')
     set_gpio "$postcode"
     echo "set-post-code-led initial postcode: $postcode"
 fi
