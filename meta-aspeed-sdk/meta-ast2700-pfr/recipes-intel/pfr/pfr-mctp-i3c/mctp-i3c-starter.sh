50e0c060d6026ba14b1c9e7b8d7dd99d70fb957d
Vince Chang <vince_chang@aspeedtech.com>
2025-12-03 10:25:44 +0800

    meta-aspeed-sdk:ast2700-dcscm: pfr-mctp-i3c add mctp bridge mode
    - Refactored script to use mode selection variable (PFR_MCTP_I3C_MODE)
    for easier switching between CPU emulation and bridge modes.
    - MCTP bridge uses kernel sockets to forward MCTP packets between two
    nterfaces. Currently, the Linux kernel socket interface does not
    support I3C slave mode, so only PFR 5.0 is supported here.
    
    Tested at AvenueCtiy:
    Send Get MCTP Message Type from AST1060 and get CPU response.
    
    ```
    uart:~$ mctp send_i3c cpu 9 1d 0 80 5
    request:
    00000000: 00 80 05
    response:
    00000000: 00 00 05 00 02 05 7e
    ```
    
    Signed-off-by: Vince Chang <vince_chang@aspeedtech.com>
    Change-Id: Ia55c5e03d3138f9458d3c34ae2f815f4e5661475

--- meta-aspeed-sdk/meta-ast2700-pfr/recipes-intel/pfr/pfr-mctp-i3c/mctp-i3c-starter.sh
+++ meta-aspeed-sdk/meta-ast2700-pfr/recipes-intel/pfr/pfr-mctp-i3c/mctp-i3c-starter.sh
@@ -18,6 +18,20 @@
 #  │           EID=0x1D ├─────────────────┤EID=0x08             │
 #  │                    │                 │                     │
 #  └────────────────────┘                 └─────────────────────┘
+#
+# MCTP Bridge Mode for PFR-5.0:
+# ┌───────────┐       ┌────────────────────┐       ┌───────────┐
+# │    CPU    │       │      AST2700       │       │  AST1060  │
+# │           │       │    MCTP Bridge     │       │           │
+# │           │       │                    │       │           │
+# │    I3C_MNG│  I3C  │mctpi3c5   mctpi3c4 │  I3C  │i3c2       │
+# │   EID=0x1D├───────┤EID=0x09   EID=0x0A ├───────┤EID=0x0B   │
+# │           │       │net=4               │       │net=4      │
+# └───────────┘       └────────────────────┘       └───────────┘
+
+# PFR MCTP I3C MODE: "CPU_EMULATION" or "BRIDGE_MODE"
+PFR_MCTP_I3C_MODE="CPU_EMULATION"
+#PFR_MCTP_I3C_MODE="BRIDGE_MODE"
 
 SetupEndpoint()
 {
@@ -31,7 +45,7 @@ GetPlatformState()
 {
 	result="0x$(aspeed-pfr-tool -r 0x0a)"
 	result=$(( result & 0x22 ))
-	if [[ $result -eq 0x22 ]]; then
+	if [ "$result" -eq $((0x22)) ]; then
 		# update PlatformState property
 		busctl get-property xyz.openbmc_project.PFR.Manager \
 		/xyz/openbmc_project/pfr xyz.openbmc_project.State.Boot.Platform \
@@ -47,17 +61,11 @@ GetPlatformState()
 	fi
 }
 
-if [ -f /tmp/.mctp_i3c_done ];then
-	exit 0
-fi
-
-if mctp link|grep mctpi3c4 > /dev/null;then
-	echo "Running PFR-5.0 MCTP over I3C Master"
-	mctp address add 0x1d dev mctpi3c4
-	mctp link set mctpi3c4 net 4 up mtu 68
+WaitForPlatformReady()
+{
 	STATE=$(GetPlatformState)
-	while true;do
-		if [ "$STATE" = "T0 BMC booted" ] || [ "$STATE" = "T0 boot complete" ];then
+	while true; do
+		if [ "$STATE" = "T0 BMC booted" ] || [ "$STATE" = "T0 boot complete" ]; then
 			SetupEndpoint
 			break
 		fi
@@ -65,13 +73,73 @@ if mctp link|grep mctpi3c4 > /dev/null;then
 		sleep 2
 		STATE=$(GetPlatformState)
 	done
-	/usr/bin/pfr-mctpd -s &
-elif [ -r /dev/i3c-mctp-target-0 ];then
-	echo "Running PFR-4.0 MCTP over I3C Target"
-	/usr/bin/pfr-mctpd -d /dev/i3c-mctp-target-0
-else
-	echo "No I3C MCTP device found"
-	exit 1
+}
+
+StartCpuEmulationMode()
+{
+	if mctp link|grep mctpi3c4 > /dev/null;then
+		echo "Running CPU Emulation for PFR-5.0 MCTP over I3C Master"
+		mctp link set mctpi3c4 net 4 up mtu 68
+		mctp addr add 0x1d dev mctpi3c4
+		WaitForPlatformReady
+		/usr/bin/pfr-mctpd -s &
+	elif [ -r /dev/i3c-mctp-target-0 ];then
+		echo "Running CPU Emulation for PFR-4.0 MCTP over I3C Target"
+		/usr/bin/pfr-mctpd -d /dev/i3c-mctp-target-0
+	else
+		echo "No I3C MCTP device found"
+		exit 1
+	fi
+	systemctl start i3c-attestation-emu.service
+}
+
+StartMCTPBridgeMode()
+{
+	if mctp link|grep mctpi3c4 > /dev/null;then
+		echo "Setup MCTP bridge over I3C to PFR"
+		mctp link set mctpi3c4 net 4 up mtu 68
+		mctp addr add 0x0a dev mctpi3c4
+		WaitForPlatformReady
+	fi
+
+	# This is a workaround to ensure the CPU I3C device is available.
+	echo "Waiting for CPU I3C device to be ready..."
+	sleep 90
+	for retry_count in $(seq 1 10); do
+		if [ -e /sys/bus/i3c/devices/5-20a012900ef ]; then
+			echo "CPU I3C found after $retry_count attempts"
+			break
+		else
+			echo "CPU I3C not found, rescanning I3C MNG bus (attempt $retry_count)"
+			echo 1 > /sys/bus/i3c/devices/14c25000.i3c5/rescan
+			sleep 10
+		fi
+	done
+	if [ ! -e /sys/bus/i3c/devices/5-20a012900ef ]; then
+		echo "Warning: CPU I3C not found after 10 attempts"
+	fi
+
+	if mctp link|grep mctpi3c5 > /dev/null;then
+		echo "Setup MCTP bridge over I3C to CPU"
+		mctp link set mctpi3c5 net 4 up mtu 68
+		mctp addr add 0x09 dev mctpi3c5
+		mctp route add 0x1d via mctpi3c5
+		mctp neigh add 0x1d dev mctpi3c5 lladdr 0x02:0a:01:29:00:ef
+	fi
+
+	#ls /sys/bus/i3c/devices/
+	#mctp-client net 4 eid 0x1d type control data 80 05
+}
+
+if [ -f /tmp/.mctp_i3c_done ];then
+	exit 0
 fi
-systemctl start i3c-attestation-emu.service
+
+if [ "$PFR_MCTP_I3C_MODE" = "CPU_EMULATION" ]; then
+	StartCpuEmulationMode
+else
+	echo "Running MCTP I3C Bridge Mode"
+	StartMCTPBridgeMode
+fi
+
 touch /tmp/.mctp_i3c_done
